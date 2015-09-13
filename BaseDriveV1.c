8cf838343405c5881bf7285d60c5b9388ce0c8b3
John <thesupplier8888@gmail.com>
2015-09-13 23:01:31 +1200

    Remove junk, add pause, comment

--- BaseDriveV1.c
+++ BaseDriveV1.c
@@ -8,56 +8,19 @@
 
 //Competition Control and Duration Settings
 #pragma competitionControl(Competition)
-#pragma autonomousDuration(20)
-#pragma userControlDuration(120)
 
 #include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-//                          Pre-Autonomous Functions
-//
-// You may want to perform some actions before the competition starts. Do them in the
-// following function.
-//
-/////////////////////////////////////////////////////////////////////////////////////////
-
 void pre_auton()
 {
-  // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
-  // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
   bStopTasksBetweenModes = true;
 
-	// All activities that occur before the competition starts
-	// Example: clearing encoders, setting servo positions, ...
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-//                                 Autonomous Task
-//
-// This task is used to control your robot during the autonomous phase of a VEX Competition.
-// You must modify the code to add your own robot specific commands here.
-//
-/////////////////////////////////////////////////////////////////////////////////////////
 
 task autonomous()
 {
-  // .....................................................................................
-  // Insert user code here.
-  // .....................................................................................
-
-	AutonomousCodePlaceholderForTesting();  // Remove this function call once you have "real" code.
+	
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-//                                 User Control Task
-//
-// This task is used to control your robot during the user control phase of a VEX Competition.
-// You must modify the code to add your own robot specific commands here.
-//
-/////////////////////////////////////////////////////////////////////////////////////////
 
 task usercontrol()
 {
@@ -65,15 +28,13 @@ task usercontrol()
 
 	while (true)
 	{
-	  // This is the main execution loop for the user control program. Each time through the loop
-	  // your program should update motor + servo values based on feedback from the joysticks.
-
-	  // .....................................................................................
-	  // Insert user code here. This is where you use the joystick values to update your motors, etc.
-	  // .....................................................................................
-	motor[leftWheel1] = vexRT(Ch3);
-	motor[leftWheel2] = vexRT(Ch3);
-	motor[rightWheel1] = vexRT(Ch2);
-	motor[rightWheel2] = vexRT(Ch2);
+		//tank drive
+		motor[leftWheel1] = vexRT(Ch3);
+		motor[leftWheel2] = vexRT(Ch3);
+		motor[rightWheel1] = vexRT(Ch2);
+		motor[rightWheel2] = vexRT(Ch2);
+		
+		//Anywhere from 25-50 Msec pause
+		wait1Msec(30); 
 	}
 }
